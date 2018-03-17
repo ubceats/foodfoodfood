26559f747285570f3e9041e8acb85f87e5a4fe06
Andrea Gottardo <andrea@gottardo.me>
2018-03-17 15:51:30 -0700

    Starting to implement AddBrand feature.

--- private/routes.php
+++ private/routes.php
@@ -7,5 +7,10 @@ $app->get('/about', \ubceats\routes\AboutThis::class);
 $app->get('/list', \ubceats\routes\AllVenues::class);
 $app->get('/search/{thing}', \ubceats\routes\Search::class);
 $app->get('/vote/{foodItemId}/{direction}', \ubceats\routes\Vote::class);
+
+// Login/logout
 $app->get('/login', \ubceats\routes\Login::class);
 $app->get('/logout', \ubceats\routes\Logout::class);
+
+// Administration options
+$app->get('/addbrand', \ubceats\routes\AddBrand::class);
