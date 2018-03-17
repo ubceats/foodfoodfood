fe7b9ee0acc2ac8ce812cf23f755a93c6e1b7989
Andrea Gottardo <andrea@gottardo.me>
2018-03-17 16:51:52 -0700

    Add brand works!

---
+++ src/ubceats/db/BrandInsertion.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ubceats\db;
+
+class BrandInsertion extends DbQuery
+{
+    private $name;
+    private $url;
+    private $desc;
+
+    public function __construct(string $name, string $url, string $desc)
+    {
+        $this->name = $name;
+        $this->url = $url;
+        $this->desc = $desc;
+    }
+
+    /**
+     * @return \mysqli_result|boolean For successful SELECT, SHOW, DESCRIBE or
+     * EXPLAIN queries <b>mysqli_query</b> will return
+     * a <b>mysqli_result</b> object.For other successful queries <b>mysqli_query</b> will
+     * return true and false on failure.
+     */
+    public function pushBrandToDB() {
+        $result = $this->query("INSERT INTO brand (name, url, `desc`) VALUES ('".$this->name."', '".$this->url."', '".$this->desc."');");
+        return $result;
+    }
+
+
+    public function runQuery()
+    {
+        // FUCK THIS SHIT!!!
+    }
+
+
+
+}
--- src/ubceats/routes/AddBrand.php
+++ src/ubceats/routes/AddBrand.php
@@ -4,14 +4,36 @@ namespace ubceats\routes;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use ubceats\db\BrandInsertion;
+use ubceats\db\Sorts\PriceSort;
 
 class AddBrand extends GenericRoute
 {
     public function __invoke(Request $request, Response $response, array $args) {
-        // Sample log message
+        // Log the page load.
         $this->container->get('logger')->info("ubceats '/' add brand");
-
-        // Render index view
-        return $this->container->get('renderer')->render($response, 'addbrand.phtml', $args);
+        $error = null;
+        if ($request->getParam("name")) {
+            // the request is an insertion!
+            $didInsert = true;
+            $name = $request->getParam("name");
+            $url = $request->getParam("url");
+            $desc = $request->getParam("desc");
+            $insertion = new BrandInsertion($name, $url, $desc);
+            $insertionResult = $insertion->pushBrandToDB();
+            if (!$insertionResult) {
+                // insertion failed, let's set the error
+                $error = mysqli_error($insertion->getDb());
+            }
+        } else {
+            $didInsert = false;
+            $name = null;
+        }
+        // Render the page.
+        return $this->container->get('renderer')->render($response, 'addbrand.phtml', [
+            "didInsert" => $didInsert,
+            "error" => $error,
+            "lastName" => $name
+        ]);
     }
 }
