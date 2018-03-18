01e9cf41dfa9fda5b6292b130139892e819c695d
Falkirks <me@falkirks.com>
2018-03-18 09:07:06 -0700

    Syntax highlighting on SQL And do some nifty joins in the get locations for brand (look at that cool left join)

--- src/ubceats/db/GetLocationsForBrand.php
+++ src/ubceats/db/GetLocationsForBrand.php
@@ -19,7 +19,15 @@ class GetLocationsForBrand extends DbQuery
 
     public function runQuery()
     {
-        $q = $this->query("SELECT l.address, l.latitude, l.longitude, l.name FROM locations l, occupies o WHERE l.name = o.locationName AND l.address = o.locationAddress AND o.brandName ='" . $this->name . "';");
+        $date = date('w');
+        if($date == 0){
+            $date = 7;
+        }
+
+        $q = $this->query("SELECT l.address, l.latitude, l.longitude, l.name, time2.opensAt, time2.closesAt
+FROM locations l
+INNER JOIN occupies o ON l.name = o.locationName AND l.address = o.locationAddress AND o.brandName = '{$this->getDb()->escape_string($this->name)}'
+LEFT JOIN opening_times time2 ON time2.day = {$date} AND o.brandName = time2.brandName AND o.locationName = time2.locationName AND o.locationAddress = time2.locationAddress;");
         if(is_bool($q)){
             return $q;
         }
